a516f63280f125e8ed801d62b4a308359b77d67b
abeTavarez <abeincwireless@gmail.com>
2021-10-16 21:16:34 -0400

    index for dev/ handler for prod

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+console.log(`Running script...`);
 
 const config = require('config');
 const twit = require('twit');
@@ -10,36 +11,33 @@ const Twit = new twit({
     access_token_secret: config.twitter_account.access_token_secret
 })
 
-function retweet(){
-    // module.exports.twitterBot = (event, context, callback) => {
-        let params = {
-            q:'#Trending min_retweets: 1 lang: en',
-            result_type: 'recent',
-            count: 150
-        }
-    
-        Twit.get('search/tweets', params, (err, data, response) => {
-            let tweets = data.statuses
-            if(err) {
-                for (let dat of tweets) {
-                    let retweetId = dat.id_str;
-                    Twit.post('statuses/retweet/:id', {id: retweetId}, (err, response) => {
-                        Twit.post('favorites/create', {id: retweetId}, (err, response) => {
-                            if (response) {
-                                console.log(`Post retweeted`);
-                                // return callback(null, `Post retweeted and favorited! with retweetId-${retweetId}`)
-                            }
-    
-                            if (err){
-                                console.log(`Already retweeted`);
-                                // return callback(`Already retweeted`, null)
-                            }
-                        })
-                    })
-                }
-            }
-        })
-    // }
-}
 
-retweet()
+//* ============== AWS Lambda Handler
+
+module.exports.twitterBot = (event, context, callback) => {
+    let params = {
+        q:'#Trending min_retweets: 1 lang: en',
+        result_type: 'recent',
+        count: 150
+    }
+
+    Twit.get('search/tweets', params, (err, data, response) => {
+        let tweets = data.statuses
+        if(!err) {
+            for (let dat of tweets) {
+                let retweetId = dat.id_str;
+                Twit.post('statuses/retweet/:id', {id: retweetId}, (err, response) => {
+                    Twit.post('favorites/create', {id: retweetId}, (err, response) => {
+                        if (response) {
+                            return callback(null, `Post retweeted and favorite! with retweetId-${retweetId}`)
+                        }
+
+                        if (err){
+                            return callback(`Already retweeted`, null)
+                        }
+                    })
+                })
+            }
+        }
+    })
+}
